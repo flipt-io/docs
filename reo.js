cd64d8a57ea423f9a6331e6fb068352fd75834d9
Mark Phelps <209477+markphelps@users.noreply.github.com>
2025-03-21 14:39:38 -0400

    chore: add custom domain section
    Signed-off-by: Mark Phelps <209477+markphelps@users.noreply.github.com>

--- reo.js
+++ reo.js
@@ -1,1 +1,12 @@
-!function(){var e,t,n;e="a9505926280f383",t=function(){Reo.init({clientID:"a9505926280f383"})},(n=document.createElement("script")).src="https://static.reo.dev/"+e+"/reo.js",n.defer=!0,n.onload=t,document.head.appendChild(n)}();
+!(function () {
+  var e, t, n;
+  (e = "a9505926280f383"),
+    (t = function () {
+      Reo.init({ clientID: "a9505926280f383" });
+    }),
+    ((n = document.createElement("script")).src =
+      "https://static.reo.dev/" + e + "/reo.js"),
+    (n.defer = !0),
+    (n.onload = t),
+    document.head.appendChild(n);
+})();
