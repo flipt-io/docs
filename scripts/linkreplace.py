e79aaec4660f673b57a9dc32c9dd6ed7a779ba7d
Mark Phelps <209477+markphelps@users.noreply.github.com>
2025-04-30 13:50:18 -0400

    feat: V2 in progress (#312)
    * chore: start just by adding v2 and moving cloud around
    
    Signed-off-by: Mark Phelps <209477+markphelps@users.noreply.github.com>
    
    * chore: fix urls
    
    Signed-off-by: Mark Phelps <209477+markphelps@users.noreply.github.com>
    
    * chore: fix v1 telemetry
    
    Signed-off-by: Mark Phelps <209477+markphelps@users.noreply.github.com>
    
    * feat: get v2 docs started
    
    Signed-off-by: Mark Phelps <209477+markphelps@users.noreply.github.com>
    
    * chore: turn off no-unused-vars because it gives false positives with mdx
    
    Signed-off-by: Mark Phelps <209477+markphelps@users.noreply.github.com>
    
    * chore: replace images
    
    Signed-off-by: Mark Phelps <209477+markphelps@users.noreply.github.com>
    
    * chore: fmt
    
    Signed-off-by: Mark Phelps <209477+markphelps@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Mark Phelps <209477+markphelps@users.noreply.github.com>

---
+++ scripts/linkreplace.py
@@ -0,0 +1,36 @@
+import argparse
+import os
+import re
+
+def replace_links_in_file(filepath: str, from_path: str, to_path: str):
+    with open(filepath, 'r', encoding='utf-8') as f:
+        content = f.read()
+
+    pattern = re.compile(r'(!?\[.*?\])\((%s(?:/[^)]*)?)\)' % re.escape(from_path.rstrip('/')))
+    replaced = pattern.sub(lambda m: f'{m.group(1)}({to_path.rstrip("/")}{m.group(2)[len(from_path.rstrip("/")):]})', content)
+
+    if content != replaced:
+        with open(filepath, 'w', encoding='utf-8') as f:
+            f.write(replaced)
+        print(f"Updated: {filepath}")
+
+def walk_and_process(root_dir: str, from_path: str, to_path: str):
+    for dirpath, _, filenames in os.walk(root_dir):
+        for fname in filenames:
+            if fname.endswith('.mdx'):
+                replace_links_in_file(os.path.join(dirpath, fname), from_path, to_path)
+
+def main():
+    parser = argparse.ArgumentParser(description="Replace markdown links that start at a root path.")
+    parser.add_argument('--from', dest='from_path', required=True, help="Root-relative path to replace (e.g., /v1)")
+    parser.add_argument('--to', dest='to_path', required=True, help="Replacement path (e.g., /v2)")
+    parser.add_argument('--dir', dest='directory', default='.', help="Directory to process recursively (default: current)")
+    args = parser.parse_args()
+
+    walk_and_process(args.directory, args.from_path, args.to_path)
+
+if __name__ == '__main__':
+    main()
+
+
+# python linkreplace.py --from /v1 --to /v2 --dir ./docs
