9ea045685270adb4b0bcbc27a7135432d00e5b03
Mark Phelps <209477+markphelps@users.noreply.github.com>
2026-03-01 16:59:52 -0500

    docs: address PR review feedback for secrets provider docs
    - Revert unrelated changes to docs/reo.js (vendor script)
    - Revert unrelated Prettier reformatting in LaunchDarkly migration guide
    - Add href links to provider cards for in-page navigation
    - Add AWS_SESSION_TOKEN for temporary credentials (STS, assumed roles)
    - Clarify that cloud provider secret keys use exact names (no paths/versions)
    - Apply Prettier formatting
    
    Signed-off-by: Mark Phelps <209477+markphelps@users.noreply.github.com>

--- docs/reo.js
+++ docs/reo.js
@@ -1,6 +1,6 @@
 !(function () {
   var e, t, n;
-  ((e = "a9505926280f383"),
+  (e = "a9505926280f383"),
     (t = function () {
       Reo.init({ clientID: "a9505926280f383" });
     }),
@@ -8,5 +8,5 @@
       "https://static.reo.dev/" + e + "/reo.js"),
     (n.defer = !0),
     (n.onload = t),
-    document.head.appendChild(n));
+    document.head.appendChild(n);
 })();
